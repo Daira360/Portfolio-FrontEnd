912b0008beb911d047d0c408c826790bc355ae77
Daira <barretodairatamara@gmail.com>
2023-02-05 14:48:09 -0300

    Commit 13

--- js/main.js
+++ js/main.js
@@ -2,6 +2,18 @@
 let sound = new Audio();
 sound.src = "./sounds/click1.mp3";
 
-/*Sonido - Gato*/
-let soundcat = new Audio();
-soundcat.src = "./sounds/cat_1.mp3";
+
+/*Menú Navbar - X*/
+let menu = document.querySelector("#menu-icon");
+menu.onclick = () => {
+    menu.classList.toggle("bx-x");
+}
+
+
+/*Skills - Close*/
+let skillsclose = document.querySelector("#skills_close");
+
+skillsclose.onclick = () => {
+    skillsclose.classList.toggle("bx-chevron-up");
+}
+
